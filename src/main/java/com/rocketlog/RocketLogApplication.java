25112df1f32a437648f24c3fa35efe1f2e7dd9b1
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-06-26 23:39:24 -0300

    Criado projeto pelo spring.initializer

---
+++ src/main/java/com/rocketlog/RocketLogApplication.java
@@ -0,0 +1,13 @@
+package com.rocketlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketLogApplication.class, args);
+	}
+
+}
