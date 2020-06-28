83f88766c2daea53d95c81a24aed278b0856c52c
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-06-28 15:50:38 -0300

    Buildes para inicialização das classes de testes

---
+++ src/test/java/com/rocketlog/builders/UserBuilder.java
@@ -0,0 +1,29 @@
+package com.rocketlog.builders;
+
+import com.rocketlog.model.entity.User;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.UUID;
+
+public class UserBuilder {
+
+    private BCryptPasswordEncoder bCrypt;
+    private User user;
+
+    public static UserBuilder admin() {
+        UserBuilder builder = new UserBuilder();
+        builder.bCrypt = new BCryptPasswordEncoder();
+        builder.user = User.builder()
+                .id(UUID.randomUUID())
+                .email("admin@admin.com")
+                .fullName("Admin")
+                .password(builder.bCrypt.encode(("admin")))
+                .build();
+        return builder;
+    }
+
+    public User build(){
+        return user;
+    }
+
+}
