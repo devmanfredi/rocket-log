8b0b86a8492230da7c2e838bdbd845ebebedbf7d
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-06-28 15:51:52 -0300

    Testes da UserRepository

---
+++ src/test/java/com/rocketlog/repository/UserRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.rocketlog.repository;
+
+import com.rocketlog.builders.UserBuilder;
+import com.rocketlog.model.entity.User;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository repository;
+
+    @Test
+    public void dadoUsuario_quandoSalvar_entaoDeveRetornarEmail() {
+        User user = UserBuilder.admin().build();
+        User result = repository.save(user);
+
+        Assert.assertNotNull(result.getEmail());
+        Assert.assertThat(result.getEmail(), Matchers.equalTo(user.getEmail()));
+
+    }
+}
