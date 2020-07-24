3e53fb7f6b89ef7d9e959c9494c60ccc4662dfd4
Heuler Felipe Ramos Manfredi <heuler.manfredi@projuris.com.br>
2020-07-24 05:25:03 -0300

    correção da migration para consultar password no h2 database

--- src/main/resources/db/migration/V0001__create_schema_initial.sql
+++ src/main/resources/db/migration/V0001__create_schema_initial.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
 	id uuid NOT NULL,
 	full_name varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
 	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
