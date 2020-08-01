464be43a682aa9634423ea8a17b174e090d25221
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-31 21:31:08 -0300

    comportamento de salvar usuário no banco e validação de email

--- src/main/java/com/rocketlog/service/UserService.java
+++ src/main/java/com/rocketlog/service/UserService.java
@@ -1,14 +1,42 @@
 package com.rocketlog.service;
 
+import com.rocketlog.dto.request.UserRequestDTO;
+import com.rocketlog.exception.ResourceExistsException;
+import com.rocketlog.mapper.UserMapper;
 import com.rocketlog.model.entity.User;
 import com.rocketlog.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Service
 public class UserService extends AbstractService<UserRepository, User, UUID> {
     public UserService(UserRepository repository) {
         super(repository);
+    }
+
+    private CustomerService customerService;
+    private UserMapper mapper;
+    private BCryptPasswordEncoder bCrypt;
+
+
+    @Transactional
+    public User save(UserRequestDTO userRequestDTO) {
+        validEmailExists(userRequestDTO.getEmail());
+        User user = mapper.map(userRequestDTO);
+        user.setPassword(bCrypt.encode(userRequestDTO.getPassword()));
+        user = repository.saveAndFlush(user);
+        customerService.save(user);
+        return user;
+    }
+
+    private void validEmailExists(String email) {
+        if (isEmailExists(email)) throw new ResourceExistsException("Email já cadastrado");
+    }
+
+    private Boolean isEmailExists(String email) {
+        return repository.findByEmail(email).isPresent();
     }
 }
