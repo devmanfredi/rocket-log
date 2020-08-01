4c52ee88301f314feccd43f5b39a685d55b0b1bc
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-08-01 14:36:10 -0300

    TESTES - Desenvolvimento da geração do token

---
+++ src/test/java/com/rocketlog/controller/OauthControllerTest.java
@@ -0,0 +1,76 @@
+package com.rocketlog.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rocketlog.service.UserService;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class OauthControllerTest {
+
+    private static String URI = "/oauth";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${security.oauth2.client.client-id}")
+    private String client;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String secret;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private JacksonJsonParser parser = new JacksonJsonParser();
+    private String token = "";
+
+    @Before
+    public void beforeTests() throws Exception {
+        token = generateToken();
+    }
+
+    private String generateToken() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", "admin@admin.com");
+        params.add("password", "admin");
+
+        ResultActions login = mvc.perform(
+                post("/oauth/token")
+                        .params(params)
+                        .accept("application/json;charset=UTF-8")
+                        .with(httpBasic(client, secret)))
+                .andExpect(status().isOk());
+
+        String token = parser.parseMap(login
+                .andReturn()
+                .getResponse()
+                .getContentAsString()).get("access_token").toString();
+
+        return String.format("Bearer %s", token);
+
+    }
+
+}
