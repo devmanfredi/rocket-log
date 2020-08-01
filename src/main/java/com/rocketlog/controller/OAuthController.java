4be41380f91184a9874c87c0d46168d50520aa4f
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-07-31 22:29:48 -0300

    documentação do controller para retornar dados do usuário autenticado

--- src/main/java/com/rocketlog/controller/OAuthController.java
+++ src/main/java/com/rocketlog/controller/OAuthController.java
@@ -4,6 +4,7 @@ import com.rocketlog.dto.request.UserRequestDTO;
 import com.rocketlog.dto.response.UserResponseDTO;
 import com.rocketlog.exception.ApiError;
 import com.rocketlog.mapper.UserMapper;
+import com.rocketlog.model.entity.User;
 import com.rocketlog.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,10 +12,8 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -40,6 +39,21 @@ public class OAuthController {
     @PostMapping(value = "/signup", produces = MediaType.APPLICATION_JSON_VALUE)
     private UserResponseDTO save(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         return userMapper.map(userService.save(userRequestDTO));
+    }
+
+    @ApiOperation(
+            value = "Recupera dados do usuário autenticado",
+            notes = "Método utilizado para recuperar dados do usuário autenticado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = UserResponseDTO.class),
+            @ApiResponse(code = 400, message = "Requisição mal formatada", response = ApiError.class),
+            @ApiResponse(code = 500, message = "Erro na api", response = ApiError.class)
+    })
+    @GetMapping(value = "/self", produces = MediaType.APPLICATION_JSON_VALUE)
+    private UserResponseDTO self() {
+        User userAuth = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userMapper.map(userService.findByEmail(userAuth.getEmail()).get());
     }
 
 }
