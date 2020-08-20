d77300f6ba6b2d123d6063ccf538471db0643cf9
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-08-20 20:36:10 -0300

    endpoint para retornar usuário pelo id

--- src/main/java/com/rocketlog/controller/UserController.java
+++ src/main/java/com/rocketlog/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.rocketlog.controller;
 
+import com.rocketlog.model.entity.User;
+import com.rocketlog.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -11,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"Users"}, description = "Rndpoint para gerenciamento dos usuários")
 public class UserController {
 
+    private UserService service;
+
+    @GetMapping("users/{id}")
+    private Optional<User> findById(@PathVariable UUID id) {
+        return service.findById(id);
+    }
 }
