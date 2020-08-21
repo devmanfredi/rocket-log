5906e510585fe25916e62f1154204477343b0bcf
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-08-20 21:56:46 -0300

    REST - Endpoint para retornar usuários com paginação

--- src/main/java/com/rocketlog/controller/UserController.java
+++ src/main/java/com/rocketlog/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.rocketlog.controller;
 
+import com.rocketlog.exception.MessageException;
 import com.rocketlog.model.entity.User;
 import com.rocketlog.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,16 @@ public class UserController {
     @GetMapping("users/{id}")
     private Optional<User> findById(@PathVariable UUID id) {
         return service.findById(id);
+    }
+
+    @GetMapping("/users")
+    private Page<User> findAll() throws MessageException {
+        PageRequest pageRequest = PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "createdDate"));
+        Page<User> userList = service.findAll(pageRequest);
+        if (userList.isEmpty()) {
+            throw new MessageException("Lista Vazia!");
+        }
+
+        return userList;
     }
 }
