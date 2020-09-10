aa63f9c3ad569e16a2ee56fe52e7beb8d7f42c58
Heuler Felipe Ramos Manfredi <heuler.manfredi@projuris.com.br>
2020-09-09 23:35:53 -0300

    TESTES - teste para retornar customer pelo usuário

--- src/test/java/com/rocketlog/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rocketlog/repository/CustomerRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.rocketlog.repository;
 
 import com.rocketlog.builders.CustomerBuilder;
+import com.rocketlog.builders.UserBuilder;
+import com.rocketlog.exception.MessageException;
 import com.rocketlog.model.entity.Customer;
+import com.rocketlog.model.entity.User;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +15,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertThat;
+
+;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -24,7 +31,7 @@ public class CustomerRepositoryTest {
     private CustomerRepository repository;
 
     @Test
-    public void dadoUsuario_quandoSalvar_deveRetornarDadosDoCustomer() {
+    public void dadoUsuario_quandoSalvar_entaoDeveRetornarDadosDoCustomer() {
         Customer customer = CustomerBuilder.customer().build();
 
         Mockito.when(repository.save(customer)).thenReturn(customer);
@@ -33,6 +40,20 @@ public class CustomerRepositoryTest {
 
         assertThat(result, Matchers.notNullValue());
         assertThat(result.getId(), Matchers.equalTo(customer.getId()));
+    }
+
+    @Test
+    public void dadoUsuario_quandoPesquisarPeloUsuario_entaoDeveRetornarCustomer() throws MessageException {
+        Customer customer = CustomerBuilder.customer().build();
+        User user = UserBuilder.comum("Heuler Manfredi").build();
+
+        Mockito.when(repository.findByUser(user)).thenReturn(Optional.of(customer));
+
+        Customer result = repository.findByUser(user).orElseThrow(() -> new MessageException("Customer Não encontrado"));
+
+        assertThat(result, Matchers.notNullValue());
+        assertThat(result.getUser().getFullName(), Matchers.equalTo(user.getFullName()));
+        assertThat(result.getApiKey(), Matchers.notNullValue());
     }
 
 }
